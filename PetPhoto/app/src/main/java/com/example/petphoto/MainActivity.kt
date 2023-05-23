19f04192d78e7bc08ed44124ba5c121bd73c692e
misung <voielactee7@naver.com>
2023-05-23 20:01:32 +0900

    직접풀어보기 4-4 완성

--- PetPhoto/app/src/main/java/com/example/petphoto/MainActivity.kt
+++ PetPhoto/app/src/main/java/com/example/petphoto/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.petphoto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.CompoundButton
@@ -18,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var rg : RadioGroup
     lateinit var linear : LinearLayout
     lateinit var imgv : ImageView
+    lateinit var btnFinish : Button
+    lateinit var btnFirst : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         checkStart = findViewById(R.id.check_start)
         rg = findViewById(R.id.rg)
         linear = findViewById(R.id.linear)
+        btnFinish = findViewById(R.id.btn_finish)
+        btnFirst = findViewById(R.id.btn_first)
         imgv = findViewById(R.id.imgv)
 
         checkStart.setOnCheckedChangeListener { CompoundButton, b ->
@@ -43,5 +48,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        btnFinish.setOnClickListener (btnListener)
+        btnFirst.setOnClickListener (btnListener)
+
+    }
+
+    var btnListener = OnClickListener {
+        when(it.id) {
+            R.id.btn_finish -> finish()
+            R.id.btn_first -> linear.visibility = View.INVISIBLE
+        }
     }
 }
