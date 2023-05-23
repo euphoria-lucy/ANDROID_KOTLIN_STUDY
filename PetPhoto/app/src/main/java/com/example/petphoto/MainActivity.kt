a2f99bf133acbfe2bc3c36430f95ea7b53c1d757
misung <voielactee7@naver.com>
2023-05-23 19:33:13 +0900

    직접풀어보기 4-4 시작함 Switch로 변경, 선택완료버튼 삭제

--- PetPhoto/app/src/main/java/com/example/petphoto/MainActivity.kt
+++ PetPhoto/app/src/main/java/com/example/petphoto/MainActivity.kt
@@ -9,14 +9,14 @@ import android.widget.CompoundButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RadioGroup
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var checkStart : CheckBox
+    lateinit var checkStart : Switch
     lateinit var rg : RadioGroup
     lateinit var linear : LinearLayout
-    lateinit var btnDone : Button
     lateinit var imgv : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         checkStart = findViewById(R.id.check_start)
         rg = findViewById(R.id.rg)
         linear = findViewById(R.id.linear)
-        btnDone = findViewById(R.id.btn_done)
         imgv = findViewById(R.id.imgv)
 
         checkStart.setOnCheckedChangeListener { CompoundButton, b ->
@@ -36,13 +35,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        btnDone.setOnClickListener {
+        rg.setOnCheckedChangeListener{ CompoundButton, b ->
             when(rg.checkedRadioButtonId) {
-                R.id.radio_dog ->imgv.setImageResource(R.drawable.puppy)
-                R.id.radio_cat ->imgv.setImageResource(R.drawable.cat)
-                R.id.radio_rabbit ->imgv.setImageResource(R.drawable.rabbit)
-                else -> Toast.makeText(applicationContext, "라디오버튼이 선택되지 않았습니다.", Toast.LENGTH_SHORT)
+                R.id.radio_dog -> imgv.setImageResource(R.drawable.puppy)
+                R.id.radio_cat -> imgv.setImageResource(R.drawable.cat)
+                R.id.radio_rabbit -> imgv.setImageResource(R.drawable.rabbit)
             }
         }
+
     }
 }
