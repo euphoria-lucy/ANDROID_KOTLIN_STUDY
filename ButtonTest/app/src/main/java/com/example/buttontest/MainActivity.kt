f4a2d847a2a3571994d8086e8446a6ba991075b2
misung <voielactee7@naver.com>
2023-05-27 20:40:36 +0900

    Revert "ANDROID_KOTLIN"
    This reverts commit 9cdc1f0ee3e63d30033322f58e918d6cfb8bfcd1.

--- ButtonTest/app/src/main/java/com/example/buttontest/MainActivity.kt
+++ ButtonTest/app/src/main/java/com/example/buttontest/MainActivity.kt
@@ -2,13 +2,10 @@ package com.example.buttontest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var btn1: Button
-        btn1 = findViewById<Button>(R.id.btn1)
     }
 }
