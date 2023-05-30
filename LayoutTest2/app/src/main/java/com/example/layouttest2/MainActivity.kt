2c18bc4ea516761b752b5936bab19a4802f05d60
misung <voielactee7@naver.com>
2023-05-30 19:38:23 +0900

    클래스에서 button위젯 생성 및 추가

--- LayoutTest2/app/src/main/java/com/example/layouttest2/MainActivity.kt
+++ LayoutTest2/app/src/main/java/com/example/layouttest2/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.layouttest2
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         linear1.setBackgroundColor(Color.rgb(220, 211, 231))
         val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
         setContentView(linear1, params)
+
+        val btn = Button(this)
+        btn.text = "아기짐승 김정민"
+        btn.setBackgroundColor(Color.rgb(240, 219, 225))
+        linear1.addView(btn)
+
+        btn.setOnClickListener {
+            Toast.makeText(applicationContext, "말랑콩떡 김정민", Toast.LENGTH_LONG).show()
+        }
 
     }
 }
