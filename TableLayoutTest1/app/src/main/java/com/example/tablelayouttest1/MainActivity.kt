a09dda9007d8eaef20ec00f5aaf98fb86e0589fa
misung <voielactee7@naver.com>
2023-06-13 19:52:22 +0900

    [ANDROID_KOTLIN] 테이블레이아웃으로 계산기 만들기_최종

--- TableLayoutTest1/app/src/main/java/com/example/tablelayouttest1/MainActivity.kt
+++ TableLayoutTest1/app/src/main/java/com/example/tablelayouttest1/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.tablelayouttest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,6 +35,42 @@ class MainActivity : AppCompatActivity() {
         btnMultiply = findViewById(R.id.btnMultiply)
         btnDivide = findViewById(R.id.btnDivide)
         textResult = findViewById(R.id.textResult)
+        btnPlus.setOnClickListener(btnListener)
+        btnMinus.setOnClickListener(btnListener)
+        btnMultiply.setOnClickListener(btnListener)
+        btnDivide.setOnClickListener(btnListener)
 
+        for (i in 0..btnIds.size - 1 step 1) {
+            numButtons.add(findViewById(btnIds[i]))
+        }
+
+        for (i in 0..btnIds.size - 1 step 1) {
+            numButtons[i].setOnClickListener {
+                if (edit_o.isFocused) {
+                    num_o = edit_o.text.toString() + numButtons[i].text.toString()
+                    edit_o.setText(num_o)
+                } else if (edit_t.isFocused) {
+                    num_t = edit_t.text.toString() + numButtons[i].text.toString()
+                    edit_t.setText(num_t)
+                } else {
+                    Toast.makeText(applicationContext, "먼저 입력할 곳에 포커스를 주세요", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
+    var btnListener = OnClickListener {
+        num_o = edit_o.text.toString()
+        num_t = edit_t.text.toString()
+
+        when(it.id) {
+            R.id.btnPlus -> result = num_o.toInt() + num_t.toInt()
+            R.id.btnMinus -> result = num_o.toInt() - num_t.toInt()
+            R.id.btnMultiply -> result = num_o.toInt() * num_t.toInt()
+            R.id.btnDivide -> result = num_o.toInt() / num_t.toInt()
+        }
+        textResult.setText(R.string.text_result)
+        textResult.append(result.toString())
     }
 }
