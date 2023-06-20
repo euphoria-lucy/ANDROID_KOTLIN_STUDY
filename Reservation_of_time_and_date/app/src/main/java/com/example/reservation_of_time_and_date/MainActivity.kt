147d36b61a03c19ec01a1587e2fccdde7d1a458a
misung <voielactee7@naver.com>
2023-06-20 19:53:52 +0900

    [ANDROID_KOTLIN] 날짜, 시간 예약 앱_layout 완성

--- Reservation_of_time_and_date/app/src/main/java/com/example/reservation_of_time_and_date/MainActivity.kt
+++ Reservation_of_time_and_date/app/src/main/java/com/example/reservation_of_time_and_date/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.reservation_of_time_and_date
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.Chronometer
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.TimePicker
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var chrono_one : Chronometer
+    lateinit var btnStart : Button
+    lateinit var btnDone : Button
+    lateinit var rg : RadioGroup
+    lateinit var calendar : CalendarView
+    lateinit var timePicker : TimePicker
+    lateinit var textResult : TextView
+    var selectedYear : Int = 0
+    var selectedMonth : Int = 0
+    var selectedDay : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        chrono_one = findViewById(R.id.chrono_one)
+        btnStart = findViewById(R.id.btnStart)
+        btnDone = findViewById(R.id.btnDone)
+        rg = findViewById(R.id.rg)
+        calendar = findViewById(R.id.calendar)
+        timePicker = findViewById(R.id.timePicker)
+        textResult = findViewById(R.id.textResult)
+
+        timePicker.visibility = View.INVISIBLE
+        calendar.visibility = View.INVISIBLE
+
     }
 }
