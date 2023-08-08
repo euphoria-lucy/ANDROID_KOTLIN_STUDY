656d34423b05e876c1ac4ec88783bc03195da423
misung <voielactee7@naver.com>
2023-08-08 19:37:13 +0900

    [ANDROID_KOTLIN] ViewFlipper

--- ViewFlipperTest/app/src/main/java/com/example/viewflippertest/MainActivity.kt
+++ ViewFlipperTest/app/src/main/java/com/example/viewflippertest/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.viewflippertest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ViewFlipper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnPrev = findViewById<Button>(R.id.btnPrev)
+        var btnNext = findViewById<Button>(R.id.btnNext)
+        var flipper = findViewById<ViewFlipper>(R.id.flipper_one)
+
+        btnPrev.setOnClickListener {
+            flipper.showPrevious()
+        }
+
+        btnNext.setOnClickListener {
+            flipper.showNext()
+        }
+
     }
 }
