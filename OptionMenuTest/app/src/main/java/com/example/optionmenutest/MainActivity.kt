a781a696b1b4a54a39b98472e04c0c6a62c3061a
misung <voielactee7@naver.com>
2023-08-24 19:16:28 +0900

    [ANDROID_KOTLIN] OptionMenu

--- OptionMenuTest/app/src/main/java/com/example/optionmenutest/MainActivity.kt
+++ OptionMenuTest/app/src/main/java/com/example/optionmenutest/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.optionmenutest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.LinearLayout
 
@@ -26,6 +28,45 @@ class MainActivity : AppCompatActivity() {
         var mInflater = menuInflater
         mInflater.inflate(R.menu.menu1, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.itemRed -> {
+                linear.setBackgroundColor(Color.parseColor("#F4DDE2"))
+                return true
+            }
+
+            R.id.itemBlue -> {
+                linear.setBackgroundColor(Color.parseColor("#DADFEC"))
+                return true
+            }
+
+            R.id.itemMagenta -> {
+                linear.setBackgroundColor(Color.parseColor("#DED8EB"))
+                return true
+            }
+
+            R.id.subRotate -> {
+                linear.setBackgroundColor(Color.parseColor("#F4DDE2"))
+                btn.rotation += 45f
+                return true
+            }
+
+            R.id.subSize -> {
+                btn.scaleX = 2f
+                return true
+            }
+
+            R.id.subInit -> {
+                btn.rotation = 0f
+                btn.scaleX = 1f
+                return true
+            }
+        }
+
+        return false
+
     }
 
 }
