e8348ba560df87dfdae5f819342a69eecfb92722
misung <voielactee7@naver.com>
2023-08-31 21:03:36 +0900

    [ANDROID_KOTLIN] menu

--- ContextMenuTest/app/src/main/java/com/example/contextmenutest/MainActivity.kt
+++ ContextMenuTest/app/src/main/java/com/example/contextmenutest/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.contextmenutest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
@@ -42,6 +44,43 @@ class MainActivity : AppCompatActivity() {
             mInflater.inflate(R.menu.menu_two, menu)
         }
 
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return super.onContextItemSelected(item)
+        when(item.itemId) {
+            R.id.itemPink -> {
+                linear.setBackgroundColor(Color.parseColor("#F4DDE2"))
+                return true;
+            }
+
+            R.id.itemSkyBlue -> {
+                linear.setBackgroundColor(Color.parseColor("#DADFEC"))
+                return true;
+            }
+
+            R.id.itemPurple -> {
+                linear.setBackgroundColor(Color.parseColor("#DED8EB"))
+                return true;
+            }
+
+            R.id.subRotate -> {
+                btn_one.rotation = 180f
+                return true
+            }
+
+            R.id.subSize -> {
+                btn_one.scaleX = 2f
+                return true
+            }
+
+            R.id.subOriginal -> {
+                btn_one.rotation = 0f
+                btn_one.scaleX = 1f
+                return true
+            }
+        }
+        return false
     }
 
 }
