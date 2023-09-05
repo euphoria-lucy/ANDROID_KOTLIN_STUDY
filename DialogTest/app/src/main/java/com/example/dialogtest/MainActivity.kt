5f714771034cbf7f8f8b6917a599c371f6268841
misung <voielactee7@naver.com>
2023-09-05 19:34:43 +0900

    [ANDROID_KOTLIN] DialogTest

--- DialogTest/app/src/main/java/com/example/dialogtest/MainActivity.kt
+++ DialogTest/app/src/main/java/com/example/dialogtest/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.dialogtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnItem : Button
+    lateinit var textResult : TextView
+    lateinit var imgv : ImageView
+    var verrionArray = arrayOf("김정민", "류현곤", "박성한")
+    var baseballImgRes = arrayOf(R.drawable.img_one, R.drawable.img_two, R.drawable.img_thr)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnItem = findViewById(R.id.btn_item)
+        textResult = findViewById(R.id.text_result)
+        imgv = findViewById<ImageView>(R.id.imgv)
+
+        btnItem.setOnClickListener {
+            var dlg = AlertDialog.Builder(this@MainActivity)
+            dlg.setTitle("좋아하는 야구선수는?")
+            dlg.setIcon(R.drawable.baseball)
+            dlg.setItems(verrionArray) { dialog, index ->
+                textResult.text = verrionArray[index]
+                imgv.setImageResource(baseballImgRes[index])
+            }
+            dlg.setNegativeButton("닫기", null)
+            dlg.show()
+        }
     }
 }
