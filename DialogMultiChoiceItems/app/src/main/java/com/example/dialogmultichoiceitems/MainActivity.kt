4ef6890b8593a5f0f7451ea065283b0f8bf5e9a5
misung <voielactee7@naver.com>
2023-09-05 20:03:19 +0900

    [ANDROID_KOTLIN] DialogMultiChoiceItems 완성 전

--- DialogMultiChoiceItems/app/src/main/java/com/example/dialogmultichoiceitems/MainActivity.kt
+++ DialogMultiChoiceItems/app/src/main/java/com/example/dialogmultichoiceitems/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.dialogmultichoiceitems
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.btn)
+
+        btn.setOnClickListener {
+            var baseballArray = arrayOf("김정민", "류현곤", "박성한")
+            var checkArray = booleanArrayOf(false, true, false)
+            var dlg = AlertDialog.Builder(this@MainActivity)
+            dlg.setTitle("좋아하는 야구선수는?")
+            dlg.setIcon(R.drawable.baseball)
+            dlg.setMultiChoiceItems(baseballArray, checkArray) {
+                dialog, index, isChecked ->
+                btn.text = baseballArray[index]
+            }
+            dlg.setNegativeButton("닫기", null)
+            dlg.show()
+        }
+
     }
 }
