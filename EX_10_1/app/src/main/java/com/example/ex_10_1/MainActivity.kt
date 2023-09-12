7216c8f8914445263bf37d0202392b93a88cca6e
misung <voielactee7@naver.com>
2023-09-12 20:04:08 +0900

    [ANDROID_KOTLIN] 직접 풀어보기 완성

--- EX_10_1/app/src/main/java/com/example/ex_10_1/MainActivity.kt
+++ EX_10_1/app/src/main/java/com/example/ex_10_1/MainActivity.kt
@@ -4,19 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnMain : Button
+    lateinit var rg : RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnMain = findViewById(R.id.btnMain)
+        rg = findViewById(R.id.rg)
+
         btnMain.setOnClickListener {
             var intent = Intent(applicationContext, SecondActivity::class.java)
-            startActivity(intent)
+            if (rg.checkedRadioButtonId == R.id.rb_two) {
+                intent = Intent(applicationContext, SecondActivity::class.java)
+            } else {
+                intent = Intent(applicationContext, ThirdActivity::class.java)
+            }
+
         }
+
     }
 }
---
+++ EX_10_1/app/src/main/java/com/example/ex_10_1/ThirdActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ex_10_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ThirdActivity : AppCompatActivity() {
+
+    lateinit var btnThird : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+
+        btnThird = findViewById(R.id.btnThird)
+        btnThird.setOnClickListener {
+            finish()
+        }
+
+    }
+}
